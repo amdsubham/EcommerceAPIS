d210593bb8df986d83e8626528814eeec5afb78b
Subham <Subham@gmail.com>
2023-06-05 13:16:07 +0530

    Changes

--- controllers/admin/orders.js
+++ controllers/admin/orders.js
@@ -1,24 +1,24 @@
 const ordersModel = require("../../models/order")
 
 module.exports.getAllOrders = async (req, res) => {
-    try{
+    try {
 
         const orders = await ordersModel.find()
-            .populate({path : "user" , select : "-password -token"})
+            .populate({ path: "user", select: "-password -token" })
             .populate("items.productId")
             .populate("items.categoryId")
 
         const ordersCount = await ordersModel.find().count()
 
         return res.json({
-            success : true,
-            message : "all orders",
-            status : 200,
-            data : orders,
+            success: true,
+            message: "all orders",
+            status: 200,
+            data: orders,
             ordersCount
         })
 
-    }catch(error){
+    } catch (error) {
 
         return res.send(error.message)
     }
@@ -37,44 +37,43 @@ module.exports.getCurrentDate = async () => {
 }
 
 module.exports.changeStatusOfOrder = async (req, res) => {
-    try{
+    try {
 
-        const {status, orderId} = req.query
+        const { status, orderId } = req.query
 
-        if(!orderId || !status){
+        if (!orderId || !status) {
             return res.json({
-                success : false,
-                message : "status or order Id is missing"
+                success: false,
+                message: "status or order Id is missing"
             })
         }
-        if(!["delivered","pending","shipped"].includes(status)){
+        if (!["delivered", "pending", "shipped"].includes(status)) {
             return res.json({
-                success : false,
-                message : "wrong status"
+                success: false,
+                message: "wrong status"
             })
         }
 
         var today = await this.getCurrentDate()
-        
+
         var statusUpdate;
-        if(status == "shipped"){
-            statusUpdate = await ordersModel.findOneAndUpdate({_id : orderId},{status : status, shippedOn : today}, {new : true})
+        if (status == "shipped") {
+            statusUpdate = await ordersModel.findOneAndUpdate({ _id: orderId }, { status: status, shippedOn: today }, { new: true })
         }
-        else if(status == "delivered"){
-            statusUpdate = await ordersModel.findOneAndUpdate({_id : orderId},{status : status, deliveredOn : today}, {new : true})
+        else if (status == "delivered") {
+            statusUpdate = await ordersModel.findOneAndUpdate({ _id: orderId }, { status: status, deliveredOn: today }, { new: true })
         }
-        else{
-            statusUpdate = await ordersModel.findOneAndUpdate({_id : orderId},{status : status}, {new : true})
+        else {
+            statusUpdate = await ordersModel.findOneAndUpdate({ _id: orderId }, { status: status }, { new: true })
         }
 
         return res.json({
-            success : true,
-            message : "status updated successfully",
-            status : 200,
-            data : statusUpdate
+            success: true,
+            message: "status updated successfully",
+            status: 200,
+            data: statusUpdate
         })
-
-    }catch(error){
+    } catch (error) {
         return res.send(error.message)
     }
 }
